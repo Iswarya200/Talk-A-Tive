74b5aa79eb826b4cb50d353ea2c3e9fcaa62ba42
Kedharisetti Iswarya Lakshmi <143804342+Iswarya200@users.noreply.github.com>
2025-06-18 19:07:47 +0530

    Update server.js

--- TALK-A-TIVE-main/backend/server.js
+++ TALK-A-TIVE-main/backend/server.js
@@ -24,23 +24,6 @@ app.use("/api/user", userRoutes);
 app.use("/api/chat", chatRoutes);
 app.use("/api/message", messageRoutes);
 
-// --------------------------deployment------------------------------
-
-// const __dirname1 = path.resolve();
-
-// if (process.env.NODE_ENV === "production") {
-//   app.use(express.static(path.join(__dirname1, "/frontend/build")));
-
-//   app.get("*", (req, res) =>
-//     res.sendFile(path.resolve(__dirname1, "frontend", "build", "index.html"))
-//   );
-// } else {
-//   app.get("/", (req, res) => {
-//     res.send("API is running..");
-//   });
-// }
-
-// --------------------------deployment------------------------------
 
 app.use(notFound);
 app.use(errorHandler);
@@ -83,6 +66,7 @@ io.on("connection", (socket) => {
     });
   });
   socket.off("setup", () => {
+    
     console.log("USER DISCONNECTED");
     socket.leave(userData._id);
   });
